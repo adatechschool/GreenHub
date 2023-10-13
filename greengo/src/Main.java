5c687d82aba200e7ac3a9dbb2f313a6876171e10
MehdiGrimault <mehdi.grimault@gmail.com>
2023-10-13 15:41:23 +0200

    test de la fonction qui renvoit la meilleure destination co2 en avion jusqu'a 5 voyageurs

--- greengo/src/Main.java
+++ greengo/src/Main.java
@@ -1,63 +1,93 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Main {
     public static City nantes = new City("Nantes", 0, 0);
     public static City lyon = new City("Lyon", 0, 0);
     public static City paris = new City("Paris", 0, 0);
+    public static City bordeaux = new City("Bordeaux", 0, 0);
+    public static City toulouse = new City("Toulouse", 0, 0);
+
+    public static City allCities[] = {nantes,lyon,paris,bordeaux,toulouse};
+
+    /*-----------------*/
 
     public static Trip trip1 = new Trip(nantes,nantes,0);
     public static Trip trip2 = new Trip(nantes,lyon,538);
     public static Trip trip3 = new Trip(nantes,paris,372);
-    public static Trip trip4 = new Trip(lyon,nantes,538);
-    public static Trip trip5 = new Trip(lyon,lyon,0);
-    public static Trip trip6 = new Trip(lyon,paris,412);
-    public static Trip trip7 = new Trip(paris,nantes,372);
-    public static Trip trip8 = new Trip(paris,lyon,412);
-    public static Trip trip9 = new Trip(paris,paris,0);
+    public static Trip trip4 = new Trip(nantes,bordeaux,267);
+    public static Trip trip5 = new Trip(nantes,toulouse,455);
 
-    public static Trip allTrips[] = {trip1,trip2,trip3,trip4,trip5,trip6,trip7,trip8,trip9};
+    /*-----------------*/
+
+    public static Trip trip6 = new Trip(lyon,nantes,538);
+    public static Trip trip7 = new Trip(lyon,lyon,0);
+    public static Trip trip8 = new Trip(lyon,paris,412);
+    public static Trip trip9 = new Trip(lyon,bordeaux,465);
+    public static Trip trip10 = new Trip(lyon,toulouse,375);
+
+
+    /*-----------------*/
+    public static Trip trip11 = new Trip(paris,nantes,372);
+    public static Trip trip12 = new Trip(paris,lyon,412);
+    public static Trip trip13 = new Trip(paris,paris,0);
+    public static Trip trip14 = new Trip(paris,bordeaux,526);
+    public static Trip trip15 = new Trip(paris,toulouse,604);
+
+    /*-----------------*/
+
+    public static Trip trip16 = new Trip(bordeaux,nantes,267);
+    public static Trip trip17 = new Trip(bordeaux,lyon,465);
+    public static Trip trip18 = new Trip(bordeaux,paris,526);
+    public static Trip trip19 = new Trip(bordeaux,toulouse,213);
+    public static Trip trip20 = new Trip(bordeaux,bordeaux,0);
+
+    /*-----------------*/
+
+    public static Trip trip21 = new Trip(toulouse,paris,604);
+    public static Trip trip22 = new Trip(toulouse,nantes,455);
+    public static Trip trip23 = new Trip(toulouse,lyon,375);
+    public static Trip trip24 = new Trip(toulouse,toulouse,0);
+    public static Trip trip25 = new Trip(toulouse,bordeaux,213);
+
+
+    public static Trip allTrips[] = {trip1,trip2,trip3,trip4,trip5,trip6,trip7,trip8,trip9,trip10,trip11,trip12,trip13,trip14,trip15,trip16,trip17,trip18,trip19,trip20,trip21,trip22,trip23,trip24,trip25};
 
     public static Travelers traveler1 = new Travelers(nantes,1);
     public static Travelers traveler2 = new Travelers(lyon,1);
     public static Travelers traveler3 = new Travelers(paris,1);
+    public static Travelers traveler4 = new Travelers(bordeaux,1);
+    public static Travelers traveler5 = new Travelers(toulouse,1);
 
-    public static Travelers groupOfTravelers[] = {traveler1,traveler2,traveler3};
+    /*public static Travelers groupOfTravelers[] = {traveler1,traveler2,traveler3,traveler4,traveler5};*/
 
 
 
     public static void main(String[] args) {
-        getLowestCO2Destination(groupOfTravelers);
+
     }
 
 
-    public static HashMap<City,Integer> getLowestCO2Destination(Travelers [] groupOfTravelers) {
+    public static Entry<City,Integer> getLowestCO2Destination(Travelers [] groupOfTravelers) {
         HashMap<City,Integer> SumC02PerDestination = new HashMap<>();
-        SumC02PerDestination.put(nantes, 0);
-        SumC02PerDestination.put(lyon, 0);
-        SumC02PerDestination.put(paris, 0);
+        for (City city : allCities){
+            SumC02PerDestination.put(city, 0);
+        }
 
-        // for each traveller get the C02 emission for each destination
         HashMap<City,Integer> CO2PerCity = new HashMap<>();
-        for (int i = 0;i < 3; i++) {
-            CO2PerCity.putAll(groupOfTravelers[i].getCO2PerDestination());
-            CO2PerCity.forEach((key, value) -> System.out.println(key.getName() + " = " + value));
-            System.out.println(CO2PerCity.get(lyon));
-
+        for (Travelers traveler : groupOfTravelers){
+            // for each traveller get the C02 emission for each destination
+            CO2PerCity.putAll(traveler.getCO2PerDestination());
             // for each destination calculate the global C02 emissions
             CO2PerCity.forEach((city, CO2) -> SumC02PerDestination.put(city, SumC02PerDestination.get(city) + CO2));
         }
-        System.out.println("lyon :" + SumC02PerDestination.get(lyon));
-        System.out.println("Paris :" + SumC02PerDestination.get(paris));
-        System.out.println("Nantes :" + SumC02PerDestination.get(nantes));
-
 
         // return the lowest CO2 emission destination & the value
-        
-        HashMap<City,Integer> result = new HashMap<>();
-
-
-        return result;
+        Entry<City,Integer>  entryWithMinValue = Collections.min(SumC02PerDestination.entrySet(), Map.Entry.comparingByValue());
+        System.out.println("Clé : " + entryWithMinValue.getKey().getName() + ", Valeur : " + entryWithMinValue.getValue());
+        return entryWithMinValue;
     }
 }
--- greengo/tests/Tests.java
+++ greengo/tests/Tests.java
@@ -1,10 +1,10 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.lang.reflect.Array;
-import java.util.Dictionary;
+import java.util.AbstractMap;
 import java.util.HashMap;
-import java.util.*;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Tests {
     @Test
@@ -18,19 +18,13 @@ public class Tests {
     public void getCO2PerDestinationTest() {
         /*create the travelers instance for test*/
         Travelers travelersTest = new Travelers(Main.nantes, 1);
-        //Create a dictionnary to store the key value / pair : city / co2
-        HashMap<City, Integer> co2PerCity = new HashMap<>();
-        co2PerCity.put(Main.nantes, 0);
-        co2PerCity.put(Main.lyon, 153);
-        co2PerCity.put(Main.paris, 106);
-        Assert.assertEquals(co2PerCity,travelersTest.getCO2PerDestination());
+        Assert.assertTrue(153 == travelersTest.getCO2PerDestination().get(Main.lyon));
     }
     @Test
     // fonction: pour chaque destination faire la somme des emissions de CO2 de tous les voyageurs & retourner la destination la + économe en C02
     public void getLowestCO2DestinationTest() {
-        HashMap<City,Integer> result = new HashMap<>();
-        result.put(Main.paris, 300);
-        Assert.assertEquals(result, Main.getLowestCO2Destination(Main.groupOfTravelers));
-
+        Travelers groupOfTravelers[] = {Main.traveler1,Main.traveler2,Main.traveler3,Main.traveler4,Main.traveler5};
+        Map.Entry<City,Integer> result = new AbstractMap.SimpleEntry<City, Integer>(Main.bordeaux,417);
+        Assert.assertEquals(result, Main.getLowestCO2Destination(groupOfTravelers));
     }
 }
