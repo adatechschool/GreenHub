280a0012d37c9a14dfbcf7527027c301c7d0e678
NathanPesneau <npesneau@MacBook-Pro-de-Pesneau.local>
2023-11-24 15:24:54 +0100

    linking openRoute WIP

---
+++ greenhub/src/main/java/com/greenhub/controllers/CarController.java
@@ -0,0 +1,25 @@
+package com.greenhub.controllers;
+import com.greenhub.models.Trip;
+import com.greenhub.services.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // to use a REST api
+@RequestMapping("/cars") // mapping corresponding to the CarController class
+public class CarController {
+
+    private final CarService carService;
+
+    @Autowired // annotation enabling injecting dependencies
+    public CarController(CarController carController) {
+        this.carService = carService;
+    }
+
+    @GetMapping("/carTrip")
+    public Trip fetchDataFromApi (@RequestParam String departureGPSCoordinates, @RequestParam String arrivalGPSCoordinates) {
+        return carService.getCarTrip(departureGPSCoordinates,arrivalGPSCoordinates);
+    }
+}
---
+++ greenhub/src/main/java/com/greenhub/services/CarService.java
@@ -0,0 +1,66 @@
+package com.greenhub.services;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.greenhub.models.City;
+import com.greenhub.models.ModeOfTransport;
+import com.greenhub.models.Car;
+import com.greenhub.models.Trip;
+import com.greenhub.models.car.CarTrips;
+import com.greenhub.models.train.TrainTrips;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class CarService {
+
+    @Value("https://api.openrouteservice.org/v2/directions/driving-car")
+    private String apiUrl;
+
+    @Value("5b3ce3597851110001cf6248f13cadea48444e5b85686de98a983a78")
+    private String apiKey;
+
+    public Trip getCarTrip(String departureGPSCoordinates, String arrivalGPSCoordinates) {
+        //create the headers of the api call
+        WebClient carApi = WebClient.builder()
+                .baseUrl(apiUrl + "?api_key=" + apiKey + "&start=" + departureGPSCoordinates + "&end=" + arrivalGPSCoordinates)
+                .defaultHeader("Content-Type", "application/json")
+                .build();
+
+        //consume the api
+        String trainApiJSON = carApi.get()
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        //create an object mapper to parse the api response to make it a trip
+        ObjectMapper om = new ObjectMapper();
+
+        try {
+            //read the trainApiJSON response and serialize it into a TrainTrips object
+            TrainTrips apiResponse = om.readValue(trainApiJSON, TrainTrips.class);
+
+            float departureX = Float.parseFloat(metadata.query.coordinates[])
+
+            ModeOfTransport car = new Car();
+            City departureCity = new City("Paris", departureX, departureY);
+            City arrivalCity = new City("Marseille", arrivalX, arrivalY);
+
+            // build the trip instance of Trip class based on elements provided by API
+            return new Trip(
+                    departureCity,
+                    arrivalCity,
+                    0,
+                    car,
+                    0,
+                    0
+            );
+
+        } catch (JsonProcessingException e) {
+            System.out.println("issue with serialization of json");
+            throw new RuntimeException(e);
+        }
+    }
+}
+
